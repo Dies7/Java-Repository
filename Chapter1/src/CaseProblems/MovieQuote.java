d17dba21edd5a851efeebf3df72b99823de52e3a
Dies7 <ed131193@students.davenport.k12.ia.us>
2017-03-29 11:07:26 -0500

    update

---
+++ Chapter1/src/CaseProblems/MovieQuote.java
@@ -0,0 +1,12 @@
+package CaseProblems;
+
+import javax.swing.JOptionPane;
+
+public class MovieQuote {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "'I did not hit her, I did not. Oh hi Mark!' - The Room");
+	}
+
+}
