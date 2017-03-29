66f48dd359ba07fe1bbbe5b6a223095d0eacf3af
Dies7 <ed131193@students.davenport.k12.ia.us>
2017-03-29 11:57:13 -0500

    update

---
+++ Chapter1/src/CaseProblems/FavoriteSong.java
@@ -0,0 +1,13 @@
+package CaseProblems;
+
+public class FavoriteSong {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Feel the city breakin' and everybody shakin'");
+		System.out.println("And we're stayin' alive, stayin' alive");
+		System.out.println("Ah, ha, ha, ha, stayin' alive, stayin' alive");
+		System.out.println("Ah, ha, ha, ha, stayin' alive");
+	}
+
+}
